d34baa9cf72c1952f0bb6b9b2a0cf887ac8786e2
Troy Ferrell <trferrel@microsoft.com>
2019-11-13 15:13:58 -0800

    Save temp stuff

---
+++ Assets/TestInstancing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInstancing : MonoBehaviour
+{
+    public Texture[] textures;
+
+    void Start()
+    {
+        int index = Random.Range(0, textures.Length - 1);
+        // Force material instance
+        GetComponent<MeshRenderer>().material.mainTexture = textures[index];
+    }
+}
